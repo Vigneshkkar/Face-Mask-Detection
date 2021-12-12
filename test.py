bdfb0facd7153bc5b0f9c4b9c5b602f46001f9aa
Vigneshkkar <vigneshkkar@gmail.com>
2021-12-12 07:41:57 -0500

    Notification System

--- test.py
+++ test.py
@@ -1,6 +1,41 @@
+import requests
+import json
+
+serverToken = 'AAAAO5T7NUo:APA91bFj3mgtfeVJUp8OM7AqVOXhvPMTjbWJpncUnD2f3jXEkxDXh3F5eBqRvxTMFyUopxQOa-kcALIiNNrjdXN4RqZzuxTF0qXhgw2OnJcTqVWPtT_qVcV_1Cbj02rWHff0w3Y90O-5'
+deviceToken = 'dRkCb37HbZTCNZo16TANhI:APA91bF6B4mzI-BXLs0UHk_wXpaRh0oUIXcqnoUph99yZwkSIgNJwuq0DaoCK84N6T888eN6n92R9U5gFUbQ_NrYdhbnPQZoaCw3CgEqxt708H8qjeKltCy2KMRx2Vz6Gdi6aPjV-wTL'
+
+headers = {
+        'Content-Type': 'application/json',
+        'Authorization': 'key=' + serverToken,
+      }
+
+body = {
+          'notification': {'title': 'Alert Hight Risk',
+                            'body': 'A Person Entered the Store without Mask.',
+                            },
+          'to':
+              deviceToken,
+          'priority': 'high',
+        #   'data': dataPayLoad,
+        }
+def send_alert():
+    response = requests.post("https://fcm.googleapis.com/fcm/send",headers = headers, data=json.dumps(body))
+    print(response.status_code)
+
+    print(response.json())
+
+
+
+
+
+
+
+
+
 import cv2
 import numpy as np
 from tensorflow.keras.models import load_model
+from time import time
 model=load_model("./model2-019.model")
 
 results={0:'without mask',1:'mask'}
@@ -12,6 +47,10 @@ cap = cv2.VideoCapture(0)
 
 haarcascade = cv2.CascadeClassifier('/Users/vigneshkkar/opt/anaconda3/lib/python3.8/site-packages/cv2/data/haarcascade_frontalface_alt.xml')
 
+check_alert = False
+alert_sent = False
+
+time_diff = time()
 
 while True:
     (rval, im) = cap.read()
@@ -36,8 +75,19 @@ while True:
         cv2.rectangle(im,(x,y),(x+w,y+h),GR_dict[label],2)
         cv2.rectangle(im,(x,y-40),(x+w,y),GR_dict[label],-1)
         cv2.putText(im, results[label], (x, y-10),cv2.FONT_HERSHEY_SIMPLEX,0.8,(255,255,255),2)
-
-    cv2.imshow('LIVE',   im)
+        print(label, type(label), check_alert)
+        if(label == 1):
+            check_alert =  False
+            alert_sent = False
+        if(label == 0 and check_alert == False):
+            print("Inside true function")
+            check_alert = True
+            time_diff = time()
+        if(int(time() - time_diff) > 5 and check_alert and alert_sent == False):
+            send_alert()
+            check_alert = False
+            alert_sent = True
+    cv2.imshow('Face Mask Detection',   im)
     key = cv2.waitKey(10)
     
     if key == 27: 
@@ -46,3 +96,5 @@ while True:
 cap.release()
 
 cv2.destroyAllWindows()
+
+
